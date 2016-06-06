76b838b93ecd15b7ef6b88a103e2e51be2de57f9
GenyLeong <genesis.leon.carbajal@gmail.com>
2016-06-06 13:32:06 -0500

    hechas y por hacer

---
+++ js/main.js
@@ -0,0 +1,51 @@
+function getInputValue() {
+	var inputBox = document.getElementById("inputBox");
+	return inputBox.value;
+}
+
+function tweet() {
+	var inputBoxValue = getInputValue();
+	if (inputBoxValue !== "") {
+		doTweet(inputBoxValue);
+		clean();
+	}
+}
+
+	
+function doTweet(tweetText) {
+
+	var elemento = document.createElement("div");
+
+	var porhacer = document.getElementById("porhacer");
+
+	//Crear el elemento
+	elemento.className = "tweet";
+	elemento.innerHTML = '<div class="checkbox boxcheck">' + '<label><input type="checkbox" onclick="hacer_click(this)"></label>' + '</div>' + tweetText + '<a class="btn btn-default pull-right" href="#" role="button" onclick="borrar(this)">' + '<i class="fa fa-trash" aria-hidden="true"></i>' + '</a>';
+
+	porhacer.appendChild(elemento);
+
+	var ex = porhacer.lastChild;
+
+}
+
+function hacer_click(elemento){
+	
+	var subrayado = elemento.parentElement.parentElement.parentElement;
+	subrayado.classList.toggle('rayados')
+	var hechas = document.getElementById("hechas");
+	hechas.appendChild(subrayado);
+}
+
+function clean() {
+	var inputBox = document.getElementById("inputBox");
+	inputBox.value = "";
+	inputBox.focus();
+}
+
+function borrar(elemento){
+
+	var tarea = elemento.parentElement;
+
+	tarea.remove();
+	
+}
