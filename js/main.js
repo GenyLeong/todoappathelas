dc60e39f355994bbe112b9750ead276625a160c2
GenyLeong <genesis.leon.carbajal@gmail.com>
2016-06-10 13:52:02 -0500

    ya cambia bien

--- js/main.js
+++ js/main.js
@@ -33,20 +33,20 @@ var contador=0;
 function hacer_click(elemento){
 	
 	var subrayado = elemento.parentElement.parentElement.parentElement;
-	// subrayado.classList.toggle('rayados')
+	
+	var hechas = document.getElementById('hechas')
+	var porhacer = document.getElementById('porhacer')
 
-		contador++;
-	if (contador%2 ==0) {
-		subrayado.classList.remove('rayados')	
-		var porhacer = document.getElementById("porhacer")
-	   	porhacer.appendChild(subrayado);
+	var x = elemento.checked;
+
+	if (x==true){
+		subrayado.classList.add('rayados')
+		hechas.appendChild(subrayado)
 	}
 
-	else {
-		// contador++;
-		subrayado.classList.add('rayados')		
-		var hechas = document.getElementById("hechas");
-		hechas.appendChild(subrayado);	
+	if (x==false){
+		subrayado.classList.remove('rayados')
+		porhacer.appendChild(subrayado)
 	}
 
 }
